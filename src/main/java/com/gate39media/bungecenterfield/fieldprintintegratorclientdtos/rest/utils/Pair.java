a2ae2f8addc88128ef6a4e288213f7ac571d7500
Renan Alves <alvees.renan@gmail.com>
2021-06-25 06:43:52 -0300

    AB#5955 - Changed path of Pair;

--- src/main/java/com/gate39media/bungecenterfield/fieldprintintegratorclientdtos/rest/utils/Pair.java
+++ src/main/java/com/gate39media/bungecenterfield/fieldprintintegratorclientdtos/rest/utils/Pair.java
@@ -1,16 +1,18 @@
-package com.gate39media.bungecenterfield.fieldprintintegratorclientdtos.rest.dtos.commons;
+package com.gate39media.bungecenterfield.fieldprintintegratorclientdtos.rest.utils;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(fluent = true)
 @NoArgsConstructor
 @AllArgsConstructor
 public class Pair<U, V> {
     U first;
     V second;
-    static <U, V> Pair<U, V> of(U u, V v) {
+    public static <U, V> Pair<U, V> of(U u, V v) {
         return new Pair<>(u, v);
     }
 }

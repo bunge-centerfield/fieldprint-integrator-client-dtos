45d3aec6aa4698b5e8e52bd43e753143ac54601c
Renan Alves <alvees.renan@gmail.com>
2021-09-14 07:01:24 -0300

    Removed ignore unknonw from parent fieldprint calculator dto;

--- src/main/java/com/gate39media/bungecenterfield/fieldprintintegratorclientdtos/rest/dtos/report/FieldprintCalculatorReportDto.java
+++ src/main/java/com/gate39media/bungecenterfield/fieldprintintegratorclientdtos/rest/dtos/report/FieldprintCalculatorReportDto.java
@@ -1,6 +1,5 @@
 package com.gate39media.bungecenterfield.fieldprintintegratorclientdtos.rest.dtos.report;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.gate39media.bungecenterfield.fieldprintintegratorclientdtos.rest.dtos.report.cropyears.CropYearsDto;
 import com.gate39media.bungecenterfield.fieldprintintegratorclientdtos.rest.dtos.report.metadata.MetadataDto;
 import lombok.AllArgsConstructor;
@@ -14,7 +13,6 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class FieldprintCalculatorReportDto {
     private MetadataDto metadata;
     private List<CropYearsDto> cropyears;

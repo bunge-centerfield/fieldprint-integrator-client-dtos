728d2abfebf12c8574dfeaf08e50001891b0c0ba
Renan Alves <alvees.renan@gmail.com>
2021-09-24 13:32:06 -0300

    Added title for fieldprint metric type enum

--- src/main/java/com/gate39media/bungecenterfield/fieldprintintegratorclientdtos/rest/dtos/report/commons/FieldprintMetricType.java
+++ src/main/java/com/gate39media/bungecenterfield/fieldprintintegratorclientdtos/rest/dtos/report/commons/FieldprintMetricType.java
@@ -1,5 +1,22 @@
 package com.gate39media.bungecenterfield.fieldprintintegratorclientdtos.rest.dtos.report.commons;
 
+import lombok.Getter;
+
+@Getter
 public enum FieldprintMetricType {
-    LAND_USE, SOIL_CONSERVATION, SOIL_CARBON, IRRIGATION_WATER, ENERGY_USE, GREENHOUSE_GAS, WATER_QUALITY, BIODIVERSITY;
+
+    LAND_USE("Land Use"),
+    SOIL_CONSERVATION("Soil Conservation"),
+    SOIL_CARBON("Soil Carbon"),
+    IRRIGATION_WATER("Irrigation Water"),
+    ENERGY_USE("Energy Use"),
+    GREENHOUSE_GAS("Greenhouse Gas"),
+    WATER_QUALITY("Water Quality"),
+    BIODIVERSITY("Biodiversity");
+
+    private final String title;
+
+    FieldprintMetricType(final String title) {
+        this.title = title;
+    }
 }
